031074d4ed71e443f13d0895c6ea6c2b5d1bfd9b
Claire <clairefraser121@gmail.com>
2024-01-02 16:13:08 +0000

    define and export the Engineer class

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,20 +1,30 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
+// Engineer.js
+
 // Import the Employee class
 const Employee = require("./Employee");
 
-//Define the Engineer class, inheriting from Employee
+// Define the Engineer class, inheriting from Employee
 class Engineer extends Employee {
-    constructor(name, id, email, github) {
-        // Call the constructor of the parent class (Employee)
-        super(name, id, email);
+  constructor(name, id, email, github) {
+    // Call the constructor of the parent class (Employee)
+    super(name, id, email);
 
-        // Additional property specific to Engineer
-        this.github = github;
-    }
+    // Additional property specific to Engineer
+    this.github = github;
+  }
 
-// Additional method specific to Engineer
-getGithub() {
-    return this.getGithub
+  // Additional method specific to Engineer
+  getGithub() {
+    return this.github;
+  }
+
+  // Override the getRole() method
+  getRole() {
+    return "Engineer";
+  }
 }
-}
+
+// Export the Engineer class
+module.exports = Engineer;
