f8f60189be697750c087dfc61d67cc8b932ac4b3
Claire <clairefraser121@gmail.com>
2024-01-02 16:13:27 +0000

    define and export the Intern class

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+// Intern.js
+
+// Import the Employee class
+const Employee = require("./Employee");
+
+// Define the Intern class, inheriting from Employee
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+        // Call the constructor of the parent class (Employee)
+        super(name, id, email);
+
+        // Additional method specific to Intern
+        this.school = school;
+    }
+
+// Additional method specific to Intern
+getSchool() {
+    return this.school;
+}
+
+// Override the getRole() method
+getRole() {
+    return "Intern";
+}
+}
+
+// Export the Intern class
+module.exports = Intern
